868cc77b77ca8413f68ce8426d7c9f6ebd997ffc
Maxi Alvarez <maxi25294@gmail.com>
2024-06-14 19:03:22 -0300

    Cambios de configuracion para docker

--- server/database/config.js
+++ server/database/config.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 
 const dbConnection = async() => {
+    console.log('MONGODB_CNN:', process.env.MONGODB_CNN);
     
     try {
-        await mongoose.connect(process.env.MONGODB_CNN);
+        await mongoose.connect(process.env.MONGODB_CNN,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
 
         console.log('Base de datos online');
 
